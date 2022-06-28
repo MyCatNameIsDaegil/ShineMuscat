e9ff50af1f0fa1f99b70e4d016ff1f7dab5a9f3f
MyCatNameIsDaegil <108320635+MyCatNameIsDaegil@users.noreply.github.com>
2022-06-28 14:45:47 +0900

    Add files via upload

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.wrap a').click(function(){
+        $('.popup').fadeIn();//.show();
+    });
+    $('.popup a').click(function(){
+        $('.popup').fadeOut();//.hide();
+    });
+});
